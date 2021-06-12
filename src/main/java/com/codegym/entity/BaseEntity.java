a245510280a1c1a8c76e3363c208d5c30579364c
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-06-12 22:35:29 +0700

    config security unfinished

--- src/main/java/com/codegym/entity/BaseEntity.java
+++ src/main/java/com/codegym/entity/BaseEntity.java
@@ -1,23 +1,34 @@
 package com.codegym.entity;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.util.Date;
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "createddate")
+    @CreatedDate
     private Date createdDate;
 
     @Column(name = "modifieddate")
+    @LastModifiedDate
     private Date modifiedDate;
 
     @Column(name = "createdBy")
+    @CreatedBy
     private String createdBy;
 
     @Column(name = "modifiedBy")
+    @LastModifiedBy
     private String modifiedBy;
 
     public Long getId() {
